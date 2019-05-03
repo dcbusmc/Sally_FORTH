935d3399f338ce3f95416bdf94a9fed17d0047b0
dcbusmc <47577557+dcbusmc@users.noreply.github.com>
2019-05-03 00:45:36 -0400

    Add files via upload

---
+++ driver2.cpp
@@ -0,0 +1,32 @@
+// File: driver2cpp
+//
+// CMSC 341 Fall 2018 Project 2
+//
+// Simple driver program to call the Sally Forth interpreter
+// This version accepts user input for filename of Sally Forth
+// source code.
+//
+
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+#include "Sally.h"
+
+int main() {
+	string fname;
+
+	cout << "Enter file name: ";
+	cin >> fname;
+	ifstream ifile(fname.c_str());
+
+	Sally S(ifile);
+
+	S.mainLoop();
+
+	ifile.close();
+
+	system("pause");
+	return 0;
+}
